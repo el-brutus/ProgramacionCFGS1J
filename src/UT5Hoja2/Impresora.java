65007c8f64f557b6c973987218e105f55babf5f2
Raul Martinez <raulmartinezmora2@gmail.com>
2026-01-27 11:52:34 +0100

    Progreso actividad 3

--- src/UT5Hoja2/Impresora.java
+++ src/UT5Hoja2/Impresora.java
@@ -1,4 +1,14 @@
 package UT5Hoja2;
 
 public class Impresora {
+    private int nivel_toner;
+    private int numpaginas;
+    private boolean impresiondoblecara;
+
+    public int cantidadtonel(int nivel_toner, int cantmaxima){
+    if (nivel_toner > cantmaxima || nivel_toner < 0){
+        return -1;
+    }
+    return nivel_toner;
+    }
 }
